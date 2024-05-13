639e3733832449ce2090c9b3a5db52db673f46a3
c9yu <polkj11290@naver.com>
2024-05-13 10:40:28 +0900

    20240513

---
+++ day08/wpf08_toyproject_app/wpf08_toyproject_app/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace wpf08_toyproject_app
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ day08/wpf08_toyproject_app/wpf08_toyproject_app/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using Microsoft.Data.SqlClient;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+
+
+namespace wpf08_toyproject_app
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+
+        private HttpClient httpClient;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            httpClient = new HttpClient();
+            LoadDataFromOpenAPI();
+        }
+
+
+
+        private void DataList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
